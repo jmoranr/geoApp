67b8bc95262b5ec15c93677dcd38297bf072f1d6
Jesus Moran <jmmoranr01@hotmail.com>
2022-04-20 10:17:43 +0200

    refactoring of the first entrypoint

--- api/app.py
+++ api/app.py
@@ -1,18 +1,44 @@
 import os
 from flask import Flask, jsonify
-import connect_db
+import model
 import re
 from datetime import datetime
 
 app = Flask(__name__)
 
+# Error messages and pattern matchings
+wrongPointErr = "Request bad formed. Please try to enter a geometry that follows this pattern: '-3.83 40.39' (long lat)"
+pointPattern = re.compile("^(-?[0-9]+.?[0-9]+ -?[0-9]+.?[0-9]+)+$")
+wrongDateErr = "Request bad formed. Please try to enter a date that follows this pattern: '2015-03-16' (yyyy-mm-dd)"
+datePattern = re.compile("^([0-9]+-?)+$")
+
+
 def conn():
-    conndb = connect_db.connect()
+    conndb = model.connect()
     if conndb:
         return conndb
     else:
         return False
 
+def pointValidation(inputPoint):
+    if not pointPattern.match(inputPoint):
+        return False
+    else:
+        return True
+
+def dateValidation(inputDate):
+    if not datePattern.match(inputDate):
+        return False
+    year, month, day = inputDate.split('-')
+    isValidDate = True
+    try:
+        datetime(int(year), int(month), int(day))
+    except ValueError:
+        isValidDate = False
+    if not isValidDate:
+        return False
+    else:
+        return True
 
 
 @app.route('/', methods=['GET'])
@@ -24,29 +50,23 @@ def welcome():
 
 
 @app.route('/<inputPoint>/<inputDate>/', methods=['GET'])
-def point(inputPoint, inputDate):
-
-    # Date validation
-    wrongDateErr = "Request bad formed. Please try to enter a date that follows this pattern: '2015-03-16'"
-    datePattern = re.compile("^([0-9]+-?)+$")
-    if not datePattern.match(inputDate):
-        return wrongDateErr
-    year, month, day = inputDate.split('-')
-    isValidDate = True
-    try:
-        datetime(int(year), int(month), int(day))
-    except ValueError:
-        isValidDate = False
-    if not isValidDate:
-        return wrongDateErr
-
-    # Point validation
-    wrongPointErr = "Request bad formed. Please try to enter a geometry that follows this pattern: '-3.83 40.39'"
-    pointPattern = re.compile("^(-?[0-9]+.?[0-9]+ -?[0-9]+.?[0-9]+)+$")
-    if not pointPattern.match(inputPoint):
-        return wrongPointErr
+def dateData(inputPoint, inputDate):
+    if pointValidation(inputPoint):
+        if dateValidation(inputDate):
+            return jsonify({
+                'title': 'TURNOVER BY AGE AND GENDER',
+                'data': model.elem1(conn(), inputPoint, inputDate)
+            })
+        else:
+            return wrongDateErr
     else:
-        return jsonify({'result': connect_db.elem1(conn(), inputPoint, inputDate)})
+        return wrongPointErr
+    
+    
+
+
+
+
 
 
 app.run(host='0.0.0.0', port=os.getenv('PORT'))
--- api/model.py
+++ api/model.py
@@ -17,18 +17,6 @@ def connect():
 def elem1(conn, point, date):   
     pointSplit = point.split()
     cur = conn.cursor()
-    # cur.execute(
-    #     """
-    #     select amount, p_month,  p_age, p_gender, postal_code_id 
-    #         from paystats 
-    #         where postal_code_id=(
-    #             select id 
-    #                 from postal_codes where st_Contains(
-    #                     st_geomfromtext(st_astext(the_geom)), st_point({long}, {lat})
-    #                 )
-    #         )
-    #     """.format(long=pointSplit[0], lat=pointSplit[1])
-    # )
     cur.execute(
         """
         select p_age as age, p_gender as gender, SUM (amount) as amount 
